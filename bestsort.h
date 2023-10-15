1ae7109a7039f3a277616309602475a20db02365
harmor <128482429+harmor123@users.noreply.github.com>
2023-10-15 12:48:06 +0800

    Add files via upload

---
+++ bestsort.h
@@ -0,0 +1,17 @@
+#ifndef BESTSORT_H
+#define BESTSORT_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <time.h>
+
+
+class bestsort
+{
+public:
+    bestsort();
+};
+
+#endif // BESTSORT_H
